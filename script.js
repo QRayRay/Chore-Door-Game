66b8f2a81951f591bfed66e0b60c1ba5eb960fac
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2021-04-23 15:40:25 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let doorImage1 = document.getElementById('door1');
+let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
+doorImage1.onclick = () => {
+doorImage1.src = botDoorPath;
+};
+
+let doorImage2 = document.getElementById('door2');
+let beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
+doorImage2.onclick = () => {
+doorImage2.src = beachDoorPath;
+};
+
+let doorImage3 = document.getElementById('door3');
+let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
+doorImage3.onclick = () => {
+doorImage3.src = spaceDoorPath;
+};
